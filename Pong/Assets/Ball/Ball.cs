a88928aff52393d10c7a6f34bdf53ef5d4d386c6
Phoebus Petsimeris <phoebuspetsi@gmail.com>
2024-07-22 20:44:07 +0300

    ball collision and reflection on top/bottom walls

--- Pong/Assets/Ball/Ball.cs
+++ Pong/Assets/Ball/Ball.cs
@@ -4,36 +4,74 @@ public class Ball : MonoBehaviour
 {
     [SerializeField] private float speed = 5;
 
-    private float randomStartAngle;
+    private SpriteRenderer spriteRenderer;
+    private Camera cam;
+
+    private float angle;
     private int mirrorDirection;
     private int movementDirection;
 
     void Start()
     {
-        randomStartAngle = Random.Range(0.1f, 2f);
+        cam = FindObjectOfType<Camera>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
-        float rand = Random.Range(0f, 1f);
-        if(rand < 0.5)
-        {
-            mirrorDirection = -1;
-        } else
-        {
-            mirrorDirection = 1;
-        }
-        rand = Random.Range(0f, 1f);
-        if(rand > 0.5)
-        {
-            movementDirection = -1;
-        } else
-        {
-            movementDirection = 1;
-        }
+        randomizeStart();
+        Debug.Log(angle);
     }
 
     void Update()
     {
+        Vector2 movement = new Vector2(speed * movementDirection, (speed * movementDirection) * angle * mirrorDirection) * Time.deltaTime;
+        
+        if(checkForBorderCollision(movement.y))
+        {
+            angle *= -1;
+            Debug.Log("new angle=" + angle);
+        }
 
-        transform.position += new Vector3(speed * randomStartAngle * mirrorDirection * movementDirection * Time.deltaTime, speed * Time.deltaTime * movementDirection, 0);
+        transform.position += new Vector3(movement.x, movement.y, 0);
+    }
+    private bool checkForBorderCollision(float movement)
+    {
+        // Get the height of the sprite and multiply it by the scale on the Y axis.
+        float ballHeight = spriteRenderer.sprite.bounds.size.y * transform.lossyScale.y;
 
+        float topEdge = transform.position.y + ballHeight / 2;
+        float bottomEdge = transform.position.y - ballHeight / 2;
+
+        // Check if topEdge will go over the screen or if bottomEdge will go under the screen.
+        if (cam.WorldToViewportPoint(new Vector3(0, topEdge + movement, 0)).y < 1
+            && cam.WorldToViewportPoint(new Vector3(0, bottomEdge + movement, 0)).y > 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void randomizeStart()
+    {
+
+        angle = Random.Range(0.1f, 2f);
+
+        float rand = Random.Range(0f, 1f);
+        if (rand < 0.5)
+        {
+            mirrorDirection = -1;
+        }
+        else
+        {
+            mirrorDirection = 1;
+        }
+        rand = Random.Range(0f, 1f);
+        if (rand > 0.5)
+        {
+            movementDirection = -1;
+        }
+        else
+        {
+            movementDirection = 1;
+        }
     }
 }
--- Pong/Assets/Paddle/Paddle.cs
+++ Pong/Assets/Paddle/Paddle.cs
@@ -5,8 +5,6 @@ public class PaddleScript : MonoBehaviour
     [SerializeField] private float speed = 10;
 
     private SpriteRenderer spriteRenderer;
-    private float paddleHeight;
-
     private Camera cam = null;
 
     void Start()
@@ -14,22 +12,34 @@ public class PaddleScript : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         cam = FindObjectOfType<Camera>();
 
-        // Get the height of the sprite and multiply it by the scale on the Y axis.
-        paddleHeight = spriteRenderer.sprite.bounds.size.y * transform.lossyScale.y;
     }
 
     void Update()
     {
-        float topEdge = transform.position.y + paddleHeight / 2;
-        float bottomEdge = transform.position.y - paddleHeight / 2;
-
+        
         float movement = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
-
-        // Check if topEdge will go over the screen or if bottomEdge will go under the screen.
-        if(cam.WorldToViewportPoint(new Vector3(0, topEdge + movement, 0)).y < 1
-            && cam.WorldToViewportPoint(new Vector3(0, bottomEdge + movement, 0)).y > 0)
+        if(!checkForBorderCollision(movement))
         {
             transform.position += new Vector3(0, movement, 0);
         }
+
+    }
+
+    private bool checkForBorderCollision(float movement)
+    {
+        // Get the height of the sprite and multiply it by the scale on the Y axis.
+        float paddleHeight = spriteRenderer.sprite.bounds.size.y * transform.lossyScale.y;
+
+        float topEdge = transform.position.y + paddleHeight / 2;
+        float bottomEdge = transform.position.y - paddleHeight / 2;
+
+        // Check if topEdge will go over the screen or if bottomEdge will go under the screen.
+        if (cam.WorldToViewportPoint(new Vector3(0, topEdge + movement, 0)).y < 1
+            && cam.WorldToViewportPoint(new Vector3(0, bottomEdge + movement, 0)).y > 0)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
